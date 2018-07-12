6c5f6d3bec12deb8c13a5d600dccd2ba4e269d0d
ImranOpenFin <imran@openfin.co>
2018-07-12 13:38:42 +0100

    Tutorial 01 Hello World

---
+++ Tutorial 01 Hello World/HelloWorld/Program.cs
@@ -0,0 +1,52 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using Newtonsoft.Json.Linq;
+using Openfin.Desktop;
+
+
+namespace HelloWorld
+{
+   public class Program
+    {
+  
+            static void Main(string[] args)
+             {
+
+                Console.WriteLine("Hello World!");
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadKey();
+
+            /*
+            var runtimeOptions = new Openfin.Desktop.RuntimeOptions
+            {
+                Version = "stable",
+                EnableRemoteDevTools = true,
+                RemoteDevToolsPort = 9090
+            };
+
+            var runtime = Openfin.Desktop.Runtime.GetRuntimeInstance(runtimeOptions);
+
+            runtime.Error += (sender, e) =>
+            {
+                Console.Write(e);
+            };
+
+            var openFinRuntime = Runtime.GetRuntimeInstance(runtimeOptions);
+            openFinRuntime.Connect(() =>
+            {
+                Utils.InvokeOnUiThreadIfRequired(this, () =>
+                Console.WriteLine("OpenFin Runtime is connected");
+
+           
+            });
+            */
+
+        }
+    }
+
+}
+
+ 
---
+++ Tutorial 01 Hello World/README.md
@@ -0,0 +1,2 @@
+
+## Tutorial 01 Hello World
